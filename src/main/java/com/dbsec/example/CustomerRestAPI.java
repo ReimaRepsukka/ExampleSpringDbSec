2ff8fefcbc16529786e4831bd9f9b2e7640913fd
repsukka <81526112+repsukka@users.noreply.github.com>
2022-03-15 15:45:08 +0200

    Security settings

--- src/main/java/com/dbsec/example/CustomerRestAPI.java
+++ src/main/java/com/dbsec/example/CustomerRestAPI.java
@@ -2,9 +2,12 @@ package com.dbsec.example;
 
 import com.dbsec.example.data.Customer;
 import com.dbsec.example.data.CustomerInfo;
+import com.dbsec.example.data.Role;
+import com.dbsec.example.security.CustomerSecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,6 +19,74 @@ public class CustomerRestAPI {
 
     @Autowired
     CustomerService customerService;
+    @Autowired
+    CustomerSecurityService customerSecurity;
+
+    /**
+     * Basic authentication login
+     * @param basicAuthHeader Basic username:password header where the credentials are  BASE64 coded.
+     * @return unauthorized status or json {"token"} if succeed
+     */
+    @PostMapping("/loginbasic")
+    public ResponseEntity<Map<String,String>> loginBasic(@RequestHeader("Authorization") String basicAuthHeader){
+
+        String token = customerSecurity.checkBasicAuthentication(basicAuthHeader);
+
+        if(token == null){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        return new ResponseEntity<>( Map.of( "token", token ), HttpStatus.OK );
+    }
+
+    /**
+     * Json login
+     * @param credentials credentials in json {"username, "password"}
+     * @return unauthorized status or json {"token", token} if succeed
+     */
+    @PostMapping("/login")
+    public ResponseEntity<Map<String,String>> login(@RequestBody Map<String,String> credentials){
+        String token = customerSecurity.checkAuthentication(
+                credentials.get("username"),
+                credentials.get("password"));
+
+        if(token == null){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>( Map.of( "token", token ), HttpStatus.OK );
+    }
+
+    /**
+     * Login with query parameters
+     * @param username
+     * @param password
+     * @return
+     */
+    @PostMapping("/loginform")
+    public ResponseEntity<Map<String,String>> loginForm(@RequestParam String username, @RequestParam String password){
+        String token = customerSecurity.checkAuthentication(username,password);
+
+        if(token == null){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>( Map.of( "token", token ), HttpStatus.OK );
+    }
+
+
+    /**
+     * Private resource that is accessible only if the token is verified
+     * @param bearer JWT token in header   Authorization: Bearer jwttoken
+     * @return authenticated customer info or forbidden status
+     */
+    @GetMapping("/private")
+    public ResponseEntity<Customer> getPrivate(@RequestHeader("Authorization") String bearer){
+        Customer c = customerSecurity.validateBearerToken(bearer);
+        if( c== null){
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        return new ResponseEntity<>(c, HttpStatus.OK);
+    }
 
     @GetMapping("/customers")
     public Map<String, Object> getCustomers(){
---
+++ src/main/java/com/dbsec/example/security/CustomerSecurityService.java
@@ -0,0 +1,137 @@
+package com.dbsec.example.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.dbsec.example.CustomerService;
+import com.dbsec.example.data.Customer;
+import com.dbsec.example.data.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Base64;
+
+@Service
+public class CustomerSecurityService {
+    @Autowired
+    CustomerService customerService;
+    @Autowired
+    CustomerPwEncoder encoder;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+
+    /**
+     * Checks basic authentication from basic auth header
+     * @param basicAuthHeader
+     * @return
+     */
+    public String checkBasicAuthentication(String basicAuthHeader){
+
+        if(!basicAuthHeader.startsWith("Basic")){
+            return null;
+        }
+
+        String cred = basicAuthHeader.substring("Basic".length()+1);
+        cred = new String(Base64.getDecoder().decode(cred));  //username:password
+
+        String[] info = cred.split(":");
+
+        return checkAuthentication(info[0], info[1]);
+    }
+
+    /**
+     * Method that authenticates and returns a jwt token if authenticated.
+     * @param username
+     * @param password
+     * @return jwt token or null
+     */
+    public String checkAuthentication(String username, String password){
+        Customer c = customerService.getCustomer(username);
+        if(c == null){
+            return null;
+        }
+
+        return encoder.matches(password, c.password ) ? createToken(c) : null;
+    }
+
+    /**
+     * Creates jwt token from customer info
+     * @param customer
+     * @return jwt token
+     */
+    public String createToken(Customer customer){
+        Algorithm alg = Algorithm.HMAC256(jwtSecret);
+
+        return JWT.create()
+                .withSubject(customer.username)
+                .withClaim("role", customer.role.toString())
+                .sign(alg);
+    }
+
+    /**
+     * Validates the bearer header that contains jwt token
+     * @param bearerHeader header containing bearer info  (Bearer jwttoken)
+     * @return customer info from jwt payload or null if not valid token
+     */
+    public Customer validateBearerToken(String bearerHeader){
+        if(bearerHeader.startsWith("Bearer")){
+            String token = bearerHeader.substring( "Bearer".length() + 1 );
+            return this.validateJwt(token);
+        }
+        return null;
+    }
+
+    /**
+     * Validates jwt token string
+     * @param jwtToken
+     * @return customer info from jwt payload or null if not valid token
+     */
+    public Customer validateJwt(String jwtToken){
+        Algorithm alg = Algorithm.HMAC256(jwtSecret);
+        JWTVerifier verifier = JWT.require(alg).build();
+
+        Customer customer = null;
+
+        try{
+            DecodedJWT jwt = verifier.verify(jwtToken);
+
+            customer = new Customer(
+                    jwt.getSubject(),
+                    null,
+                    Role.valueOf(jwt.getClaim("role").asString()));
+
+        }catch(JWTVerificationException e){}
+
+        return customer;
+    }
+
+    /**
+     * Extra method for ebabling CORS configuration if needed.
+     * @return
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
+                "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type",
+                "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
